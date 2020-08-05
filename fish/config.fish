a9ab88f764da74bd2cbebae19847710dc4faa506
charles <dinneyacharles007@gmail.com>
2020-08-05 23:26:43 +0100

    add function comment

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ alias mcp='mvn clean package'
 alias bootRun='mvn spring-boot:run'
 alias gpom='git push origin master'
 
-# functions
+# set git add, commit and push
 function commit
         if test (count $argv) -lt 1
                 echo "Error: missing commit message"
