b71c16712927ec06983ea4f82df7f2b29d8db133
charles <dinneyacharles007@gmail.com>
2020-08-06 14:08:49 +0100

    update install.sh

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,29 @@
-# fish shell configuration
-# place file in ~/.config/fish/
+# fish config
+
+# set fish shell look
+set fish_greeting \x1d
 
 # aliases
-alias g='git'
+
+# navigation aliases
+alias ..='cd ..'
+alias home='cd --'
+
+# maven aliases
 alias mci='mvn clean install'
 alias mcp='mvn clean package'
 alias bootRun='mvn spring-boot:run'
-alias gpom='git push origin master'
 
-# set git add, commit and push to origin
+# git aliases
+alias g='git'
+alias gpom='git push origin master'
 function commit
-        if test (count $argv) -lt 1
-                echo "Error: 'missing commit message'"
-                echo "Usage: commit <commit message>"
-        else
-                g add .
-                git commit -m $argv[1]
-                gpom
-        end
+	if test (count $argv) -lt 1
+		echo "Error: missing 'commit message'"
+		echo "Usage: commit <commit message>"
+	else
+		g add .
+		git commit -m $argv[1]
+		gpom
+	end
 end
