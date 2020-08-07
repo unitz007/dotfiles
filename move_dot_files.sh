1144a571deafbc4fd603a02059a678754ce900dd
charles <dinneyacharles007@gmail.com>
2020-08-07 14:52:50 +0100

    add test directory

--- move_dot_files.sh
+++ move_dot_files.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-# moves fish shell's configuration file
+file=$1
+# file check function
+function _file_check_ {
+    if [ -f "$file" ]; then
+      echo "$file exists."
+    else
+      echo "$file does not exist"
+fi
+}
+
+# TESTS
+
+# file exist test functionality
+
