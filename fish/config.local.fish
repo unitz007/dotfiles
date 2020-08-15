716f6803cf20c10adff2665df7354d10a9e0a996
charles <dinneyacharles007@gmail.com>
2020-08-15 11:51:09 +0100

    add adb aliases

--- fish/config.local.fish
+++ fish/config.local.fish
@@ -11,3 +11,11 @@ alias intellij='intellij-idea-community'
 # flutter alias
 alias fd='flutter doctor'
 alias fr='flutter run -v'
+
+# adb aliases
+alias adb=/home/charles/snap/androidsdk/current/AndroidSDK/platform-tools/adb
+function adb_connect
+        adb kill-server
+        adb tcpip 5555
+        adb connect $argv[1]
+end
