c242ecc13645e943444db60c198fcd287c0d064a
Charles <dinneyacharles007@gmail.com>
2021-07-11 16:34:06 +0100

    update

--- install.sh
+++ install.sh
@@ -15,55 +15,36 @@ curl -s "https://get.sdkman.io" | bash                                   #
 # load sdkman                                                            #
 source "$HOME/.sdkman/bin/sdkman-init.sh"                                #
 # install git                                                            #
-apt-get install git -y                                                   #
-# heroku                                                                 #
-snap install --classic heroku                                            #
-# docker                                                                 #
-snap install docker                                                      #
+apt-get install git -y                                                    #
 # fish shell                                                             #
 apt-get install fish -y                                                  #
 # tmux (terminal multiplexer)                                            #
 apt-get install tmux -y                                                  #
 # ssh                                                                    #
-apt install openssh-server -y                                            #
+#apt install openssh-server -y                                            #
 ##########################################################################
 #                         PROGRAMMING LANGUAGE(S)                        #
 ##########################################################################
 # install Java                                                           #
 apt-get install openjdk-11-jdk -y                                        #
 # install Rust                                                           #
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh           #
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh           #
 ##########################################################################
 #                         BUILD TOOLS & PACKAGE MANAGERS                 #
 ##########################################################################
 # Install build tools                                                    #
 apt-get install maven -y                                                 #
 # install gradle                                                         #
-sdk install gradle 6.5.1                                                 #
+#sdk install gradle 6.5.1                                                 #
 ##########################################################################
 #                               FRAMEWORK(S)                             #
 ##########################################################################
-# install spring boot                                                    #
-sdk install springboot                                                   #
-# install Node                                                           #
-apt-get install nodejs -y                                                #
-# install flutter                                                        #
-snap install flutter --classic                                           #
-# install androidsdk                                                     #
-snap install androidsdk                                                  #
-androidsdk "build-tools;28.0.3" "platform-tools;28.0.3"                  #
-androidsdk "system-images;android-27;google_apis_playstore;x86"          #
 ##########################################################################
 #                                 IDE(S)                                 #
-##########################################################################
-# install intelliJ IDEA                                                  #
-snap install intellij-idea-community --classic                           #
-# install vscode                                                         #
-snap install code --classic                                              #
 ##########################################################################
 #                               DATABASE(S)                              #
 ##########################################################################
 # mysql                                                                  #
-apt-get install mariadb-server --y                                       #
-mysql_secure_installation                                                #
+#apt-get install mariadb-server --y                                       #
+#mysql_secure_installation                                                #
 ##########################################################################
