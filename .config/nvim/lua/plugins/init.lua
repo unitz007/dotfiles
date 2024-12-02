a65316bb7aa543737a3e5487df01910fc961f9cb
Dinneya Charles <dinneyacharles007@gmail.com>
2024-12-02 01:02:39 +0100

    add golang config to neovim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -241,6 +241,20 @@ local default_plugins = {
       require("which-key").setup(opts)
     end,
   },
+  {
+  "ray-x/go.nvim",
+  dependencies = {  -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup()
+  end,
+  event = {"CmdlineEnter"},
+  ft = {"go", 'gomod'},
+  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+}
 }
 
 local config = require("core.utils").load_config()
