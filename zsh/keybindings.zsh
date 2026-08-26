44e9b6ba8bd2a46f3b4ebc3c817190534324bf81
unitz007 <dinneyacharles007@gmail.com>
2026-08-26 16:13:03 +0200

    Refresh tmux vi mode indicator from zsh redraw

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -7,10 +7,10 @@ export KEYTIMEOUT=1
 # Make the cursor show which mode the prompt is in:
 # insert mode = beam, normal mode = block.
 function dotsync-tmux-vi-mode {
-  [[ -n "${TMUX:-}" ]] && tmux set-option -g -q @zsh_vi_mode "$1"
+  [[ -n "${TMUX:-}" ]] && tmux set-option -g -q @zsh_vi_mode "$1" \; refresh-client -S
 }
 
-function zle-keymap-select {
+function dotsync-zle-mode-refresh {
   if [[ ${KEYMAP} == vicmd ]]; then
     echo -ne '\e[2 q'
     dotsync-tmux-vi-mode NORMAL
@@ -18,6 +18,10 @@ function zle-keymap-select {
     echo -ne '\e[6 q'
     dotsync-tmux-vi-mode INSERT
   fi
+}
+
+function zle-keymap-select {
+  dotsync-zle-mode-refresh
 }
 
 function zle-line-init {
@@ -34,6 +38,8 @@ function zle-line-finish {
 zle -N zle-keymap-select
 zle -N zle-line-init
 zle -N zle-line-finish
+autoload -Uz add-zle-hook-widget
+add-zle-hook-widget line-pre-redraw dotsync-zle-mode-refresh
 
 # Keep common Emacs-style shortcuts available while in insert mode.
 bindkey -M viins '^A' beginning-of-line
