1aa2bdf74e032b4c15a9fcf506a8304f708a6c30
unitz007 <dinneyacharles007@gmail.com>
2026-08-26 15:40:49 +0200

    Add vi-style terminal navigation

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,3 +1,35 @@
 # Key bindings
-# Currently no specific key bindings in the original .zshrc
-# This file is reserved for future key bindings
+
+# Vi-style command-line editing.
+bindkey -v
+export KEYTIMEOUT=1
+
+# Make the cursor show which mode the prompt is in:
+# insert mode = beam, normal mode = block.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]]; then
+    echo -ne '\e[2 q'
+  else
+    echo -ne '\e[6 q'
+  fi
+}
+
+function zle-line-init {
+  zle -K viins
+  echo -ne '\e[6 q'
+}
+
+function zle-line-finish {
+  echo -ne '\e[0 q'
+}
+
+zle -N zle-keymap-select
+zle -N zle-line-init
+zle -N zle-line-finish
+
+# Keep common Emacs-style shortcuts available while in insert mode.
+bindkey -M viins '^A' beginning-of-line
+bindkey -M viins '^E' end-of-line
+bindkey -M viins '^P' up-line-or-history
+bindkey -M viins '^N' down-line-or-history
+bindkey -M viins '^R' history-incremental-search-backward
