8a06b51181d975c1b723122e5bd66944007ede60
unitz007 <dinneyacharles007@gmail.com>
2026-08-26 20:03:28 +0200

    Use tmux-only mode indicator

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -4,40 +4,14 @@
 bindkey -v
 export KEYTIMEOUT=1
 
-# Publish the current zsh vi mode to tmux so the status bar can show it.
-function dotsync-tmux-vi-mode {
-  if [[ -n "${TMUX:-}" ]]; then
-    tmux set-option -gq @zsh_vi_mode "$1"
-  fi
-}
-
-function dotsync-zle-mode-refresh {
-  if [[ ${KEYMAP} == vicmd ]]; then
-    dotsync-tmux-vi-mode NORMAL
-  else
-    dotsync-tmux-vi-mode INSERT
-  fi
-}
-
-function zle-keymap-select {
-  dotsync-zle-mode-refresh
-}
-
-function zle-line-init {
-  zle -K viins
-  dotsync-tmux-vi-mode INSERT
-}
-
-function zle-line-finish {
-  dotsync-tmux-vi-mode INSERT
-}
-
-zle -N zle-keymap-select
-zle -N zle-line-init
-zle -N zle-line-finish
-zle -N dotsync-zle-mode-refresh
+# Clear older tmux mode-indicator hooks if this file is sourced in a shell
+# that loaded a previous version.
 autoload -Uz add-zle-hook-widget
 add-zle-hook-widget -d line-pre-redraw dotsync-zle-mode-refresh 2>/dev/null
+zle -D zle-keymap-select 2>/dev/null
+zle -D zle-line-init 2>/dev/null
+zle -D zle-line-finish 2>/dev/null
+zle -D dotsync-zle-mode-refresh 2>/dev/null
 
 # Keep common Emacs-style shortcuts available while in insert mode.
 bindkey -M viins '^A' beginning-of-line
